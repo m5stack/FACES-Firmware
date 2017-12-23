3d2894217d6cab7f201bcb7da74709906f8ebfd4
M5Stack <JimmyLai@M5Stack.com>
2017-12-23 19:14:39 +0800

    Add files via upload

---
+++ FacesTest.ino
@@ -0,0 +1,22 @@
+#include <Wire.h> 
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(); 
+  pinMode(5, INPUT);
+  digitalWrite(5,HIGH);
+}
+void loop()
+{
+  if (digitalRead(5) == LOW)
+  {
+    Wire.requestFrom(0x88, 1);   
+    while (Wire.available())
+    { 
+      char c = Wire.read(); // receive a byte as character
+      Serial.print(c);         // print the character
+    }
+  }
+  delay(10);
+}
